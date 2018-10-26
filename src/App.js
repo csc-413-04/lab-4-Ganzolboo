ef813dd337ae929d217bcf943b7413f67e4e8bd6
Ganzolboo Ayurzana <a.ganzolboo@yahoo.com>
2018-10-25 21:34:16 -0700

    pushing lab-4

--- src/App.js
+++ src/App.js
@@ -3,17 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
+import Header from './Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'white',
+      banner: 'hello',
+      isOpen: false,
+    };
+    this.buttonHandler = this.buttonHandler.bind(this);
+    this.textHandler = this.textHandler.bind(this);
+
+    console.log(this.props)
+    
+  }
+  onChange = () => {
+    this.setState({ color: 'grey' });
+  }
+
+  buttonHandler() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  textHandler(e) {
+    this.setState({
+      banner: e.target.value,
+    })
+  }
+
   render() {
+    let myVariable = <h2>Ganzolboo Ayurzana</h2>;
+    let myBanner;
+    if (this.state.isOpen) {
+      myBanner = <Header banner={this.state.banner}/>;
+    }
     return (
-      <div className="App">
+      <div onClick={this.buttonHandler} className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {myVariable}
           </p>
-          <h2>413 Starter Kit </h2>
+          {myBanner}
+          {
+            this.state.isOpen && 
+            <Header banner={this.state.banner}/>
+          }
+          <input value={this.state.banner} onChange={this.textHandler}/>
+          <button  onClick={this.buttonHandler} >Click Me</button>
+          <button style={{ backgroundColor: this.state.color }} onClick={this.onChange}> <p>Click to change the color of button</p></button>
         </header>
       </div>
     );
